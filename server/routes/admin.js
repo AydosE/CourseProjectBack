619966f7c7b6c24d1999a72249fadc24af5d0098
EmbergenovAJ <19ayda967198@gmail.com>
2025-07-08 13:43:25 +0500

    bug fix

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const User = require("../models/User");
+const { Op } = require("sequelize");
 
 function isAdmin(req, res, next) {
   if (req.user?.role !== "admin") {
